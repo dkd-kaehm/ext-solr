9adb55dd1a4fea1144178b7d047f644bd77cf10e
Rafael Kähm <rafael.kaehm@dkd.de>
2024-12-10 23:29:40 +0100

    [TASK] Simplify EXT:solr tests stack
    Add required methods to resolve EXT:solr PHP requirements.

--- Build/Helpers/GET_LOCAL_PACKAGE_VERSION_CONSTRAINT.sh
+++ Build/Helpers/GET_LOCAL_PACKAGE_VERSION_CONSTRAINT.sh
@@ -5,7 +5,7 @@
  * This script is made to avoid bash/system dependencies for json-parsing tools.
  * And is made for usage CI/CD usage in TYPO3-Solr addons/projects.
  */
-$addOnPath = getenv('EXT_SOLR_ADDON_PATH') ?? null;
+$addOnPath = getenv('EXT_SOLR_ADDON_PATH') ?: null;
 
 $usage = "
   Usage:
@@ -16,36 +16,41 @@ $usage = "
 ";
 
 if (!isset($argv[1])) {
-  fwrite(
-    STDERR,
-    'This script requires a package name from composer.json' .
+    fwrite(
+        STDERR,
+        'This script requires a package name from composer.json' .
     $usage
-  );
-  exit(1);
+    );
+    exit(1);
 }
 
 $composerJsonPath = ($addOnPath ?: __DIR__ . '/../..') . '/composer.json';
 
-if (false === realpath($composerJsonPath)) {
-  fwrite(
-    STDERR,
-    "No such file: {$composerJsonPath}" . PHP_EOL
-  );
-  exit(1);
+if (realpath($composerJsonPath) === false) {
+    fwrite(
+        STDERR,
+        "No such file: $composerJsonPath" . PHP_EOL
+    );
+    exit(1);
 }
 
 $composerManifest = json_decode(file_get_contents(realpath($composerJsonPath)), true);
 
 $requestedPackageName = $argv[1];
 if (isset($composerManifest['require'][$requestedPackageName])) {
-  fwrite(STDOUT, $composerManifest['require'][$requestedPackageName] . PHP_EOL);
-  exit(0);
+    fwrite(STDOUT, $composerManifest['require'][$requestedPackageName] . PHP_EOL);
+    exit(0);
 }
 
 if (isset($composerManifest['require-dev'][$requestedPackageName])) {
-  fwrite(STDOUT, $composerManifest['require-dev'][$requestedPackageName] . PHP_EOL);
-  exit(0);
+    fwrite(STDOUT, $composerManifest['require-dev'][$requestedPackageName] . PHP_EOL);
+    exit(0);
 }
 
-fwrite(STDERR, "The package \"{$requestedPackageName}\" is not defined in {$composerJsonPath}");
+if (isset($composerManifest['extra']['TYPO3-Solr']['stack-for-ci'][$requestedPackageName])) {
+    fwrite(STDOUT, $composerManifest['extra']['TYPO3-Solr']['stack-for-ci'][$requestedPackageName] . PHP_EOL);
+    exit(0);
+}
+
+fwrite(STDERR, "The package \"$requestedPackageName\" is not defined in $composerJsonPath");
 exit(10);
